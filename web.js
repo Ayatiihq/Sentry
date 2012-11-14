3946d7f2cf07841ab7cfc39caede5a2fd16ef361
Neil Jagdish Patel <njpatel@gmail.com>
2012-11-14 16:29:16 +0000

    test

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello world!');
+  response.send('Hello!');
 });
 
 var port = process.env.PORT || 8080;
