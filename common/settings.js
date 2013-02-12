4a8f8b63f04b2ce1bf57cbd509053bd1e57c1db1
Neil Jagdish Patel <njpatel@gmail.com>
2013-02-12 15:05:42 +0000

    Add a persistant settings object for get/set

---
+++ common/settings.js
@@ -0,0 +1,152 @@
+/*
+ * settings.js: store and retrieve arbritary settings key/values.
+ *
+ * Wraps the settings table.
+ *
+ * (C) 2012 Ayatii Limited
+ *
+ */
+
+var acquire = require('acquire')
+  , azure = require('azure')
+  , config = acquire('config')
+  , logger = acquire('logger').forFile('settings.js')
+  , sugar = require('sugar')
+  , util = require('util')
+  ;
+
+var TABLE = 'settings';
+
+/**
+ * Create a new settings object for the domain which should be unique to the consumer and it's
+ * arguments, so there are 'spider' level settings with domain '$spiderName', but there might be
+ * 'spider & campaign' level settings with domain '$spiderName.$campaignId'.
+ *
+ * @param {string}    domain     The domain representing the settings.
+ * @return {object}
+ */
+var Settings = module.exports = function(domain) {
+  this.tableService_ = null;
+  this.partition_ = domain ? domain : 'undefined';
+
+  this.init();
+}
+
+Settings.prototype.init = function() {
+  var self = this;
+
+  self.tableService_ = azure.createTableService(config.AZURE_CORE_ACCOUNT,
+                                                config.AZURE_CORE_KEY);
+  self.tableService_.createTableIfNotExists(TABLE, function(err) {
+    if (err)
+      logger.warn(err);
+  });
+}
+
+function defaultCallback(err) {
+  if (err)
+    logger.warn('Reply Error: %s', err);
+}
+
+
+//
+// Public Methods
+//
+/**
+ * Get all keys and values.
+ *
+ * @param {function(err, properties)}    callback    A callback to receive the properties.
+ * @return {undefined}
+ */
+Settings.prototype.getAll = function(callback) {
+  var self = this;
+
+  callback = callback ? callback : defaultCallback;
+
+  var query = azure.TableQuery.select().from(TABLE).where('PartitionKey eq ?', self.partition_);
+  
+  self.tableService_.queryEntities(query, function(err, entities) {
+    if (err) {
+      callback(err);
+      return;
+    }
+
+    var properties = {};
+    entities.forEach(function(entity) {
+      properties[entity.RowKey] = JSON.parse(entity.value);
+    });
+
+    callback(null, properties);
+  });  
+}
+
+/**
+ * Get the value of a key.
+ *
+ * @param {string}                  key         A key. Only numbers, letters and `.` allowed.
+ * @param {function(err, value)}    callback    A callback to receive the value.
+ * @return {undefined}
+ */
+Settings.prototype.get = function(key, callback) {
+  var self = this;
+
+  callback = callback ? callback : defaultCallback;
+
+  self.tableService_.queryEntity(TABLE, self.partition_, key, function(err, entity) {
+    if (err)
+      callback(err);
+    else if (entity && entity.value) {
+      callback(null, JSON.parse(entity.value));
+    } else {
+      callback();
+    }
+  });
+}
+
+/**
+ * Set the value of a key.
+ *
+ * @param {string}           key         A key. Only numbers, letters and `.` allowed.
+ * @param {any}              value       The value to set.
+ * @param {function(err)}    callback    A callback to receive and error.
+ * @return {undefined}
+ */
+Settings.prototype.set = function(key, value, callback) {
+  var self = this;
+
+  callback = callback ? callback : defaultCallback;
+
+  var entity = {};
+  entity.PartitionKey = self.partition_;
+  entity.RowKey = key;
+  entity.value = JSON.stringify(value);
+
+  self.tableService_.insertOrReplaceEntity(TABLE, entity, callback);
+}
+
+/**
+ * Set all properties for the domain.
+ *
+ * @param {object}           properties  The properties to set.
+ * @param {function(err)}    callback    A callback to receive and error.
+ * @return {undefined}
+ */
+Settings.prototype.setAll = function(properties, callback) {
+  var self = this;
+
+  callback = callback ? callback : defaultCallback;
+
+
+  self.tableService_.beginBatch();
+
+  Object.keys(properties, function(key) {
+    var entity = {};
+    entity.PartitionKey = self.partition_;
+    entity.RowKey = key;
+    entity.value = JSON.stringify(properties[key]);
+
+    self.tableService_.insertOrReplaceEntity(TABLE, entity, callback);
+  });
+
+  self.tableService_.commitBatch(callback);
+}
---
+++ tools/test_settings.js
@@ -0,0 +1,46 @@
+/*
+ * test_infringements.js: tests the infringementswrapper
+ *
+ * (C) 2012 Ayatii Limited
+ *
+ */
+
+var acquire = require('acquire')
+  , config = acquire('config')
+  , logger = acquire('logger')
+  , sugar = require('sugar')
+  ;
+
+var Settings = acquire('settings');
+
+function setupSignals() {
+  process.on('SIGINT', function() {
+    process.exit(1);
+  });
+}
+
+function main() {
+  var argv = process.argv;
+
+  logger.init();
+  logger = logger.forFile('test_infringements.js');
+
+  setupSignals();
+
+  var settings = new Settings('test');
+  var action = argv[2];
+
+  if (action === 'get')
+    settings.get(argv[3], console.log);
+
+  if (action === 'getAll')
+    settings.getAll(console.log);
+
+  if (action === 'set')
+    settings.set(argv[3], JSON.parse(argv[4]), console.log);
+
+  if (action === 'setAll')
+    settings.setAll(JSON.parse(argv[3]));
+}
+
+main();
