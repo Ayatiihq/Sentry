395dc3b1e5c6a2d68096a3f4585a73c0cbe52af5
gordallott <gord@ayatii.com>
2013-03-06 15:25:26 +0000

    skeleton

--- roles/scraper/scrapers/youtube/youtube-scraper.js
+++ roles/scraper/scrapers/youtube/youtube-scraper.js
@@ -1,1 +1,63 @@
-﻿
+﻿/*
+ * youtube.js: a youtube scraper
+ *
+ * (C) 2012 Ayatii Limited
+ *
+ * Scraper that can scrape all types of media and always takes 5mins to complete
+ * it's job. It can be paused and, if so, it will resume it's five minute
+ * timeout.
+ *
+ */
+
+var acquire = require('acquire')
+  , events = require('events')
+  , logger = acquire('logger').forFile('youtube.js')
+  , util = require('util')
+;
+
+var Scraper = acquire('scraper');
+
+var Youtube = module.exports = function () {
+  this.init();
+}
+
+util.inherits(Youtube, Scraper);
+
+Youtube.prototype.init = function () {
+  var self = this;
+}
+
+//
+// Overrides
+//
+Youtube.prototype.getName = function () {
+  return "youtube";
+}
+
+Youtube.prototype.start = function (campaign, job) {
+  var self = this;
+
+  logger.info('started for %s', campaign.name);
+  self.emit('started');
+}
+
+Youtube.prototype.stop = function () {
+  var self = this;
+  self.emit('finished');
+}
+
+Youtube.prototype.isAlive = function (cb) {
+  var self = this;
+
+  logger.info('Is alive called');
+
+  if (!self.alive)
+    self.alive = 1;
+  else
+    self.alive++;
+
+  if (self.alive > 4)
+    cb(new Error('exceeded'));
+  else
+    cb();
+}
