2acee46d53b7244ecd4f36f4d35018cb68425164
Conor Curran <conor@forwind.net>
2013-04-04 16:38:37 +0100

    point at remote selenium

--- common/spiders/isohunt/index.js
+++ common/spiders/isohunt/index.js
@@ -80,7 +80,7 @@ IsoHunt.prototype.newDriver = function(){
     self.driver.quit();
     self.driver = null;
   }
-  self.driver = new webdriver.Builder()//.usingServer('http://hoodoo.cloudapp.net:4444/wd/hub')
+  self.driver = new webdriver.Builder().usingServer('http://hoodoo.cloudapp.net:4444/wd/hub')
                                        .withCapabilities(CAPABILITIES)
                                        .build();
   self.driver.manage().timeouts().implicitlyWait(30000);
