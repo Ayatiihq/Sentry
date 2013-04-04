66f1e0ef9c38b9a46f561f887a74fa16fe651e82
Neil Jagdish Patel <njpatel@gmail.com>
2013-04-04 16:44:29 +0100

    Forgot to add files

---
+++ tools/test_analytics.js
@@ -0,0 +1,49 @@
+/*
+ * test_analytics.js: tests the analyticswrapper
+ *
+ * (C) 2012 Ayatii Limited
+ *
+ */
+
+var acquire = require('acquire')
+  , config = acquire('config')
+  , logger = acquire('logger')
+  , states = acquire('states')
+  , sugar = require('sugar')
+  ;
+
+var Campaigns = acquire('campaigns')
+  , Analytics = acquire('analytics');
+
+function setupSignals() {
+  process.on('SIGINT', function() {
+    process.exit(1);
+  });
+}
+
+function log(err) {
+  if (err)
+    console.warn(err);
+  else
+    console.log.apply(null, Object.values(arguments).slice(1));
+
+  process.exit();
+}
+
+function main() {
+  var argv = process.argv;
+
+  logger.init();
+  logger = logger.forFile('test_analytics.js');
+
+  setupSignals();
+
+  var analytics = new Analytics();
+  var action = argv[2];
+  var arg0 = argv[3];
+
+  if (action === 'getCampaignStats')
+    analytics.getCampaignStats(require(arg0), log);
+}
+
+main()
