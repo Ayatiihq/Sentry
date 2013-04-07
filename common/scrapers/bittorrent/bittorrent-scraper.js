518942bed395f4a40e46ad1f14e7c68278193a41
Conor Curran <conor@forwind.net>
2013-04-07 17:56:57 +0100

    create spidereds on search results

--- common/scrapers/bittorrent/bittorrent-scraper.js
+++ common/scrapers/bittorrent/bittorrent-scraper.js
@@ -10,6 +10,7 @@ var acquire = require('acquire')
   , webdriver = require('selenium-webdriver')
   , sugar = require('sugar')
   , cheerio = require('cheerio')
+  , URI = require('URIjs')  
 ;
 
 var Scraper = acquire('scraper');
@@ -195,10 +196,14 @@ KatScraper.prototype.beginSearch = function () {
   self.root = 'http://www.katproxy.com';
   this.remoteClient.get(self.root); 
   this.remoteClient.sleep(2000);
+  console.log('about to request : '
+              + self.root +
+              '/usearch/' +
+              self.searchTerm);
   this.remoteClient.get(self.root + '/usearch/' + self.searchTerm);
   // just submit the query for now
   // waits for a #search selector
-  this.remoteClient.findElement(webdriver.By.css('.data')).then(function gotSearchResults(element) {
+  this.remoteClient.findElement(webdriver.By.css('table.data')).then(function gotSearchResults(element) {
     if (element) {
       self.handleResults();
     }
@@ -207,17 +212,92 @@ KatScraper.prototype.beginSearch = function () {
       self.cleanup();
     }
   });
-  //self.emit('error', ERROR_NORESULTS);
-  //self.cleanup();
 };
 
 KatScraper.prototype.getLinksFromSource = function (source) {
+  var self = this;
   var links = [];
   var $ = cheerio.load(source);
-  //console.log('Search results source = ' + source);
-  /*$('.data').find('#ires').find('#rso').children().each(function () {
-    links.push($(this).find('a').attr('href'));
-  });*/
+
+  function testAttr($$, attrK, test){
+    return $$(this).attr(attrK) && $$(this).attr(attrK).match(test);
+  }
+
+  $('tr').each(function(){
+    var magnet = null;
+    var fileLink = null;
+    var torrentName = null;
+    var size = null;
+    var entityLink = null;
+    var roughDate = null;
+
+    if(testAttr.call(this, $, 'id', /torrent_*/)){
+      $(this).find('a').each(function(){
+        if(testAttr.call(this, $, 'title', /Torrent magnet link/))
+          magnet = $(this).attr('href');
+        if(testAttr.call(this, $, 'title', /Download torrent file/))
+          fileLink = $(this).attr('href');
+        // TODO there are other 'choices' which i have yet to capture - 1 in 50 or so fails.
+        if(testAttr.call(this, $, 'class', /^torType (undefined|movie|film|music)Type$/)){
+          try{
+            var inst = URI($(this).attr('href'));
+            entityLink = inst.absoluteTo(self.root).toString();
+          }
+          catch(err){
+            logger.warn('failed to create valid entity link : ' + err);
+          }
+        }
+        if(testAttr.call(this, $, 'class', 'normalgrey font12px plain bold'))
+          torrentName = $(this).text();
+      });
+      // grab the size and figure out the date.
+      $(this).find('td').each(function(){
+        if(testAttr.call(this, $, 'class', 'nobr center')){
+          size = $(this).text();
+          var age = $(this).next().next().text().trim();
+          var context = {'Minutes': age.match(/min\./),
+                         'Hours': age.match(/hour(s)?/),
+                         'Days': age.match(/day(s)?/),
+                         'Weeks': age.match(/week(s)?/),
+                         'Months': age.match(/month(s)?/),
+                         'Years': age.match(/year(s)?/)};
+          var offsetLiteral;
+          // its gotta be one and only one !            
+          Object.keys(context).each(function(key){ if(context[key] !== null) offsetLiteral = key ;});
+          var offset;
+          age.words(function(word){
+            if(parseInt(word)) 
+              offset = word;
+          })            
+          roughDate = Date.create()['add' + offsetLiteral](-offset);
+        }
+      });
+
+      if(magnet && entityLink && torrentName){
+        
+        var torrent =  new Spidered('torrent',
+                                     torrentName,
+                                     null,
+                                     entityLink,
+                                     SpideredStates.ENTITY_PAGE_PARSING);              
+        torrent.magnet = magnet;
+        torrent.fileSize = size;
+        torrent.date = roughDate;
+        torrent.directLink = fileLink; // direct link to torrent via querying torcache
+        //if(self.results.length === 0) // test with just the first (sort out sleep)
+        links.push(torrent);
+        console.log('just created : ' + JSON.stringify(torrent));
+      }
+      else{
+        logger.warn('fail to create : ' + JSON.stringify({magnet: magnet,
+                                                          fileSize: size,
+                                                          date: roughDate,
+                                                          file: fileLink,
+                                                          name: torrentName,
+                                                          link: entityLink}));
+      }
+    }
+  });
   return links;
 };
 
@@ -225,16 +305,14 @@ KatScraper.prototype.getLinksFromSource = function (source) {
 KatScraper.prototype.nextPage = function () {
   var self = this;
   // clicks the next page element.
-  self.remoteClient.findElement(webdriver.By.css('#pnnext')).click().then(function () { self.handleResults(); });
+  //self.remoteClient.findElement(webdriver.By.css('#pnnext')).click().then(function () { self.handleResults(); });
 };
 
 KatScraper.prototype.checkHasNextPage = function (source) {
   var $ = cheerio.load(source);
-  if ($('a#pnnext').length < 1) { return false; }
-  return true;
+  //if ($('a#pnnext').length < 1) { return false; }
+  return false;
 };
-
-
 
 /* Scraper Interface */
 var Bittorrent = module.exports = function () {
