d192d9c073cfeb29f51060387bc8366449c48081
Neil Jagdish Patel <njpatel@gmail.com>
2013-04-28 19:15:13 +0100

    Update deps

--- apt-deps.sh
+++ apt-deps.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-sudo add-apt-repository ppa:jon-severinsson/ffmpeg
 sudo apt-get update
-sudo apt-get install ffmpeg libboost-system1.46.1 libboost-filesystem1.46.1
+sudo apt-get install -y ffmpeg libboost-system1.46.1 libboost-filesystem1.46.1 xdg-utils
