5cbe2080faf5816ed16539df8fc982a867f61dc1
Conor Curran <conor@forwind.net>
2013-04-29 13:48:31 +0100

    got as far as i can go with 4 shared

--- common/cyberlocker-manager.js
+++ common/cyberlocker-manager.js
@@ -48,29 +48,46 @@ Cyberlocker.prototype.get = function(infringement){
 var FourShared = function () {
   var self = this;
   self.constructor.super_.call(self, '4shared.com');
-
-  var tokenLocation = 'http://www.4shared.com/v0/oauth/token';
-  var initiateLocation = 'http://www.4shared.com/v0/oauth/initiate';
-  var authorize = 'http://www.4shared.com/v0/oauth/authorize';
-
-  var state = {oauth_consumer_key: 'e4456725d56c3160ec18408d7e99f096',
-               oauth_consumer_secret: '7feceb0b18a2b3f856550e5f1ea1e979fa35d310',
-               oauth_callback: 'oob'}                      
-  oauth.requireTLS = false;
-  oauth.fetchRequestToken(state, initiateLocation, {requireTLS:false}, function(err, params){
-    console.log('oath token = ' + JSON.stringify(params));
-  });
+  self.authenticate();
   //self.root = 'http://api.4shared.com/v0/files.json?oauth_consumer_key=e4456725d56c3160ec18408d7e99f096/file/50ab8815/';
   //http://www.4shared.com/photo/q0_Jyejr/FWDP12_blog.html
 };
 
 util.inherits(FourShared, Cyberlocker);
 
+FourShared.prototype.authenticate = function(){
+  var promise = new Promise.Promise();
+
+  var initiateLocation = 'http://www.4shared.com/v0/oauth/initiate';
+  var tokenLocation = 'http://www.4shared.com/v0/oauth/token';
+  var authorizeLocation = 'http://www.4shared.com/v0/oauth/authorize';
+
+  var state = {oauth_consumer_key: 'e4456725d56c3160ec18408d7e99f096',
+               oauth_consumer_secret: '7feceb0b18a2b3f856550e5f1ea1e979fa35d310'}
+
+  oauth.requireTLS = false;
+  oauth.fetchRequestToken(state, initiateLocation, {}, function(err, params){
+    console.log('oath request token = ' + JSON.stringify(params));
+    state.oauth_token = params.oauth_token;
+    state.oauth_token_secret = params.oauth_token_secret;
+    state.oauth_verifier = '';
+
+    //params.oauth_verifier = 'niii';
+    console.log('go to authenticate with  ' + JSON.stringify(state));
+    oauth.fetchAccessToken(state, authorizeLocation, null, function(err,params){
+      console.log('oauth access token = ' + JSON.stringify(params) + err);
+    });
+  });
+  return promise;
+}
+
 FourShared.prototype.get = function(infringement){
+  var self = this;
   var URIInfrg = createURI(infringement);
   if(!URIInfrg)return;
-
-
+  self.authenticate().then(function(){
+    console.log ('finished authenticating !');
+  });
 }
 
 //-------------------------------------------------------------------------/
