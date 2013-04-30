630355b4e9b68a8048b0bb151581c039d477968b
Neil Jagdish Patel <njpatel@gmail.com>
2013-04-30 12:12:59 +0100

    Update deps

--- apt-deps.sh
+++ apt-deps.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt-get install -y ffmpeg libboost-system1.46.1 libboost-filesystem1.46.1 xdg-utils
+sudo apt-get install -y ffmpeg libboost-system1.46.1 libboost-filesystem1.46.1 xdg-utils python-pip
+sudo pip install azure
