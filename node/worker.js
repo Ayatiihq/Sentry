1fcff7a3e07a947e52c7bc151b89bca145d000f5
Neil Jagdish Patel <njpatel@gmail.com>
2013-05-04 19:45:48 +0100

    Make downloader change the db, clean up some issues with .catch

--- node/worker.js
+++ node/worker.js
@@ -101,7 +101,7 @@ Worker.prototype.setRole = function(rolename) {
     logger.warn('Role took too long to run, reaping.')
     process.exit(1);
   },
-  1000 * 60 * 90);
+  role.killTimeout ? role.killTimeout * 60 * 1000 : 1000 * 60 * 90);
 }
 
 Worker.prototype.startExit = function() {
