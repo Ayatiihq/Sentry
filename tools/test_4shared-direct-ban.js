a418b41a811a24f890cc3783c674950325d2d7eb
Conor Curran <conor@forwind.net>
2013-05-28 17:41:19 +0100

    don't use real links in the test

--- tools/test_4shared-direct-ban.js
+++ tools/test_4shared-direct-ban.js
@@ -31,7 +31,7 @@ function main() {
   logger.info('test the 4shared direct ban');
 
   var campaign = parseObject(process.argv[2]);
-  var links = ['http://www.4shared.com/download/Whrr3o58/songspkinfo_race_2_-_03_-_lat_.mp3?dsid=28gag2.b150cb6ea2205950cce6ad82386feded&tsid=20130514-052007-75388379'];//'http://example-link-1', 'http://example-link-2', 'http://example-link-3'];
+  var links = [];//'http://example-link-1', 'http://example-link-2', 'http://example-link-3'];
   var fourSharedBanner = require('../common/roles/noticesender/4shared-direct-ban');
 
   var instance = new fourSharedBanner(campaign, links);
