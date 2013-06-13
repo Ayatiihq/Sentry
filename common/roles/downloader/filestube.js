278bd90e0db01b7484d1b474ee44fa0143045a4b
Conor Curran <conor@forwind.net>
2013-06-13 11:08:25 +0100

    do the login stage

---
+++ common/roles/downloader/filestube.js
@@ -0,0 +1,64 @@
+/*
+ * filestube.js: the filestube downloader
+ *
+ * (C) 2013 Ayatii Limited
+ *
+ * Downloads direct and 'hidden' (links leading to pages with download links) links
+ * from filestube
+ *
+ */
+
+require('sugar');
+var acquire = require('acquire')
+  , cheerio = require('cheerio')
+  , fs = require('fs-extra')
+  , logger = acquire('logger').forFile('filestube.js')
+  , path = require('path')
+  , URI = require('URIjs')
+  , utilities = acquire('utilities')
+  , webdriver = require('selenium-webdriver')
+  , Seq = require('seq')  
+  , Promise = require('node-promise')    
+  ;
+
+var Filestube = module.exports = function (campaign) {
+  var self = this;
+  self.campaign = campaign;
+  self.remoteClient = null;
+};
+
+Filestube.prototype.createURI = function(uri){
+  var result = null;
+  try {
+    result = URI(uri);
+  }
+  catch (error) {
+    logger.error("Can't create uri from " + uri); // some dodgy link => move on.
+  }
+  return result;
+}
+
+Filestube.prototype.authenticate = function(){
+  var self  = this;
+
+  if(self.remoteClient){
+    logger.info('We have an active filestube session already - assume we are logged in already');
+    var promise = new Promise.Promise();
+    promise.resolve();
+    return promise;
+  }
+  self.remoteClient = new webdriver.Builder().usingServer('http://hoodoo.cloudapp.net:4444/wd/hub')
+                          .withCapabilities({ browserName: 'chrome', seleniumProtocol: 'WebDriver' }).build();
+  self.remoteClient.manage().timeouts().implicitlyWait(30000); 
+  self.remoteClient.get('http://www.filestube.com/account/login.html');
+  self.remoteClient.findElement(webdriver.By.css('[name="username"]'));
+    .sendKeys('conorayatii');
+  self.remoteClient.findElement(webdriver.By.css('[name="password"]'))
+    .sendKeys('ayatiian');
+  // xpath generated from firebug (note to self use click and not submit for such forms,
+  // submit was not able to highlight the correct input element).
+  return self.remoteClient.findElement(webdriver.By.xpath('/html/body/div/div/div[4]/div/div/form/div/div[8]/input')).click();
+}
+
+
+key = '051b6ec16152e2a74da5032591e9cc84'
