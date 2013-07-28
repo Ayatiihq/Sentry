1cc689f800fd19a94161d2918cf2de1bd8c30587
Conor Curran <conor@forwind.net>
2013-07-28 19:25:50 +0100

    add setMeta on infringments, tidy up EPW-test

--- common/infringements.js
+++ common/infringements.js
@@ -743,3 +743,23 @@ Infringements.prototype.touch = function(infringement) {
 
   self.infringements_.update({ _id: infringement._id }, { $set: { popped: Date.now() } });
 }
+
+/**
+ * Update the metadata on the infringement with the new key value pair.
+ *
+ * @infringement {object}   The infringement to update
+ * @key {string} key which to add to the metaData property
+ * @value {object} value for the new key
+ * @callback {function(err, infringements)} function to be called when the operation has completed
+ * @return  {null}
+ */
+Infringements.prototype.setMetadata = function(infringement, key, value, callback) {
+  var self = this;
+  tempMetadata = infringement.metadata ? infringement.metadata : {};
+  tempMetadata.key = value;
+  callback = callback ? callback : defaultCallback;
+
+  self.infringements_.update({ _id: infringement._id },
+                             { $set: { metadata: tempMetadata} },
+                             callback);
+}
