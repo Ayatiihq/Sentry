194a5159c72d151ad76b27def1dc69ec38faccff
Gord Allott <gord@ayatii.com>
2013-10-17 12:57:11 +0100

    strip line endings on hostname

--- tools/find-contacts.js
+++ tools/find-contacts.js
@@ -232,7 +232,7 @@ function doMainPage(uri) {
 
 var SiteInfoBuilder = function (hostname) {
   var self = this;
-  self.hostname = hostname;
+  self.hostname = hostname.strip();;
   self.emails = [];
   self.contactPages = [];
   self.hops = [];
