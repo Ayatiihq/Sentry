dba604501d96a1c0862e0ccaecc41b711adf5076
Conor Curran <conor@forwind.net>
2013-11-08 20:40:04 +0000

    hulkshare readies moved over

--- common/roles/downloader/hulkshare.js
+++ common/roles/downloader/hulkshare.js
@@ -12,16 +12,31 @@ var acquire = require('acquire')
   , Downloader = require('./downloader.js')
   ;
 
+
+
+                          .withCapabilities({ browserName: 'chrome', seleniumProtocol: 'WebDriver' }).build();
+  self.remoteClient.manage().timeouts().implicitlyWait(30000); 
+  self.remoteClient.get('http://www.Hulkshare.com/static.php?op=login');
+  self.remoteClient.findElement(webdriver.By.css('input[id=username]'))
+    .sendKeys('ayatii');
+  self.remoteClient.findElement(webdriver.By.css('input[id=password]'))
+    .sendKeys('LmpnqYc');
+  return self.remoteClient.findElement(webdriver.By.css('a#submit_button')).click();
+
+
 var Hulkshare = module.exports = function (campaign, browser) {
   var attributes = {login: {user: {'selector': 'input[id=username]',
-                                   'value' : 'ayatii'}, 
+                                   'value' : 'subscriptions@ayatii.com'},
                             password : {'selector' : 'input[id=password]',
-                                        'value' : 'LmpnqYc'},
-                            submit : 'input[id=submit_button]',
-                            at : 'http://www.hulkshare.com/static.php?op=login',
+                                        'value' : '04fd6cbb77ab088bd19015797c4f96d9'},
+                            submit : 'a[id=submit_button]',
+                            at: 'http://www.Hulkshare.com/static.php?op=login',
                             authenticated: false},
-                    targets: [],
-                    approach : states.cyberlockers.method.COW_MANGLING};
+                    targets: {available: ['a[class="bigDownloadBtn basicDownload"'],
+                              unavailable: ['div[class="playerNoTrack"]']},
+                    approach : states.downloaders.method.COWMANGLING,
+                    strategy : states.downloaders.strategy.TARGETED,
+                    blacklist : [/embed_mp3\.php/]};
   this.constructor.super_.call(this, campaign, browser, attributes);
 };
 
