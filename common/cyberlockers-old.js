58865f4df637bb3e441372153ac45ecf8d313c74
Conor Curran <conor@forwind.net>
2013-12-12 22:28:51 +0000

    to the database

---
+++ common/cyberlockers-old.js
@@ -0,0 +1,300 @@
+/*
+ * cyberlockers.js: a flat list of the domains of known cyberlockers
+ * 
+ * (C) 2013 Ayatii Limited
+ *
+ */
+
+var CyberLockers = module.exports;
+
+CyberLockers.knownDomains = [
+  '100shared.com',
+  '180upload.com',
+  '1fichier.com',
+  '247upload.com',
+  '2shared.com',
+  '4shared.com',
+  '4sync.com',
+  '4upfiles.com',
+  '1fichier.com',
+  'adrive.com',
+  'akafile.com',
+  'albafile.com',
+  'allmyvideos.net',
+  'anafile.com',
+  'anysend.com',
+  'arabloads.com',
+  'asfile.com',
+  'asuswebstorage.com',
+  'banashare.com',
+  'basicupload.com',
+  'bayfiles.com',
+  'billionuploads.com',
+  'bitshare.com',
+  'box.com',
+  'carrier.so',
+  'cloudsafe.com',
+  'cloudzer.net',
+  'cloudy.ec',
+  'cramit.in',
+  'crocko.com',
+  'cyberlocker.org',
+  'davvas.com',
+  'depositfiles.com',
+  'directmirror.com',
+  'dfiles.eu',
+  'divxstage.eu',
+  'drawpr.com',
+  'docs.google.com',
+  'drive.google.com',
+  'dropbox.com',
+  'dxr.lanedo.com',
+  'dumbeen.info',
+  'easybytez.com',
+  'embedupload.com',
+  'ex.ua',
+  'expressleech.com',
+  'extabit.com',
+  'epicshare.net',
+  'fast-file.com',
+  'fiberupload.net',
+  'file4safe.com',
+  'file4sharing.com',
+  'fileband.com',
+  'filecatalyst.com',
+  'filechum.com',
+  'filecloud.io',
+  'fileconvoy.com',
+  'filecopter.net',
+  'filedap.com',
+  'filedefend.com',
+  'fileden.com',
+  'filefactory.com',
+  'fileflyer.com',
+  'filegag.com',
+  'filekom.com',
+  'filemates.com',
+  'filenuke.com',
+  'filepost.com',
+  'fileprohost.com',
+  'filerio.com',
+  'files.com',
+  'filesanywhere.com',
+  'filesbomb.in',
+  'filesega.com',
+  'fileserving.com',
+  'filesfrom.com',
+  'fileshare.in.ua',
+  'filerio.in',
+  'filesin.com',
+  'fileslap.com',
+  'fileslap.com',
+  'filesmonster.com',
+  'filesnack.com',
+  'filesocial.com',
+  'fileom.com',
+  'filestay.com',
+  'fileswap.com',
+  'filevelocity.com',
+  'firstclass-download.com',
+  'flashx.tv',
+  'foldier.com',
+  'freakshare.com',
+  'free.fr',
+  'free4udown.com',
+  'freestorage.ro',
+  'fuupload.com',
+  'gamefront.com',
+  'ge.tt',
+  'gigasize.com',
+  'gigaup.fr',
+  'globusonline.org',
+  'go4up.com',
+  'hellshare.com',
+  'hipfile.com',
+  'hostingbulk.com',
+  'hotfile.com',
+  'hugefiles.net',
+  'hulkfile.eu',
+  'hulkshare.com',
+  'icloud.com',
+  'ifile.ws',
+  'itsuploaded.com',
+  'jumbofiles.com',
+  'jumbofiles.org',
+  'kingfiles.net',
+  'letitbit.net',
+  'limelinx.com',
+  'livedrive.com',
+  'loadpot.net',
+  'luckyshare.net',
+  'lumfile.com',
+  'magicvortex.com',
+  'mediafire.com',
+  'mega.co.nz',
+  'megaload.it',
+  'megafiles.se',
+  'megashares.com',
+  'midupload.com',
+  'mightyupload.com',
+  'mixturecloud.com',
+  'moviesmobile.net',
+  'movshare.net',
+  'movreel.com',
+  'movzap.com',
+  'mozy.com',
+  'muchshare.net',
+  'multiup.org',
+  'multiupload.nl',
+  'netload.in',
+  'nirafile.com',
+  'nosvideo.com',
+  'novafile.com',
+  'novamov.com',
+  'nowdownload.eu',
+  'nowdownload.ch',
+  'nowvideo.eu',
+  'nowvideo.ch',
+  'nowvideo.sx',
+  'obligao.com',
+  'oteupload.com',
+  'peejeshare.com',
+  'pigsonic.com',
+  'putlocker.com',
+  'playwire.com',
+  'project-free-upload.com',
+  'queenshare.com',
+  'rapidgator.net',
+  'rapidshare.com',
+  'restfile.ca',
+  'ryushare.com',
+  'saarie.com',
+  'secureupload.eu',
+  'sendmyway.com',
+  'sendspace.com',
+  'senduit.com',
+  'share-online.biz',
+  'sharebeast.com',
+  'shareblue.eu',
+  'sharefiles.co',
+  'shareflare.net',
+  'sharesend.com',
+  'skydrive.live.com',
+  'slingfile.com',
+  'sockshare.com',
+  'speedyshare.com',
+  'spideroak.com',
+  'streamcloud.org',
+  'stream-this.com',
+  'streamupload.org',
+  'sugarsync.com',
+  'swankshare.com',
+  'syncblaze.com',
+  'syncblazecloud.vembu.com',
+  'syncplicity.com',
+  'temp-share.com',
+  'turbobit.net',
+  'tusfiles.net',
+  'twindocs.com',
+  'ubuntuone.com',
+  'ul.to',
+  'uloz.to',
+  'ultramegabit.com',
+  'unlimitedshare.com',
+  'upafile.com',
+  'upload.ee',
+  'uploadboy.com',
+  'uploadbaz.com',
+  'uploadcare.com',
+  'uploaded.net',
+  'uploadhero.co',
+  'uploadic.com',
+  'uploading.com',
+  'uploadingit.com',
+  'uploadorb.com',
+  'uppit.com',
+  'upstore.net',
+  'uptobox.com',
+  'usaupload.net',
+  'verzend.be',
+  'videobam.com',
+  'videotanker.co',
+  'videoweed.es',
+  'videofox.net',
+  'vidhog.com',
+  'vidxden.com',
+  'vidto.me',
+  'vip-file.com',
+  'vreer.com',
+  'wikisend.com',
+  'wuala.com',
+  'xenubox.com',
+  'yandex.com',
+  'yousendit.com',
+  'ziddu.com',
+  'zippyshare.com',
+  'zomgupload.com',
+  'zoink.it',
+  'zuzvideo.com'
+];
+
+CyberLockers.idMatchers = {
+
+  'hostingbulk.com': {
+    domain: 'hostingbulk.com',
+    getId: function(uri) {
+      var id = uri.match(/[a-zA-Z0-9]{12}/);
+      return id ? id[0] : null;
+    }
+  },
+
+  'movshare.net': {
+    domain: 'movshare.net',
+    getId: function(uri) {
+      var id = uri.match(/[a-zA-Z0-9]{13}/);
+      return id ? id[0] : null;
+    }
+  },
+
+  'movzap.com': {
+    domain: 'movzap.com',
+    getId: function(uri) {
+      var id = uri.match(/[a-zA-Z0-9]{12}/);
+      return id ? id[0] : null;
+    }
+  },
+
+  'novamov.com': {
+    domain: 'novamov.com',
+    getId: function(uri) {
+      var id = uri.match(/[a-zA-Z0-9]{13}/);
+      return id ? id[0] : null;
+    }
+  },
+
+  'nowvideo.eu': {
+    domain: 'nowvideo.eu',
+    getId: function(uri) {
+      var id = uri.match(/[a-zA-Z0-9]{13}/);
+      return id ? id[0] : null;
+    }
+  },
+
+
+  'vidto.me': {
+    domain: 'vidto.me',
+    getId: function(uri) {
+      var id = uri.match(/[a-zA-Z0-9]{12}/);
+      return id ? id[0] : null;
+    }
+  },
+
+  'videoweed.es': {
+    domain: 'videoweed.es',
+    getId: function(uri) {
+      var id = uri.match(/[a-zA-Z0-9]{13}/);
+      return id ? id[0] : null;
+    }
+  }
+
+};
